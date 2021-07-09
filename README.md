a29645e8060219d0be6f0e8ded7a9bc360f31d53
radarsymphony <grey8knights@gmail.com>
2021-07-08 19:10:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Bash Timewarrior Extension
+This bash script works as an [extension](https://timewarrior.net/docs/api/) to the commandline time-tracker [Timewarrior](https://timewarrior.net/). The timewarrior github repo can be found: https://github.com/GothenburgBitFactory/timewarrior
+
+### Requirements/Dependencies
+ - `timew`
+ - `jq`
+
+### Usage
+1. Install timewarrior (go [here](https://timewarrior.net/docs/install/) for distro-specific options).
+2. Initialize timewarrior by running `timew` and entering `yes` when prompted to create the 'extensions' directory.
+3. Copy the `csv.sh` script to `~/.timewarrior/extensions/` and make executable (`chmod +x csv.sh`).
+4. After creating some entries, run `timew report csv.sh [:week] [tags...]` to generate a report.
+
+### Variables
+|Variable|Description|Default|
+|---|---|---|
+|`REPORT_PATH`| The location to store the report created | $HOME/timew-reports/ |
+|`USER_NAME`| The name prefixing the report | $USER|
+|`SEP`| The separator used in the CSV | ";"|
+
+
+### Additional Notes
+This one of my first projects. I created this script as an exercise. I wanted to see if I could create a bash version of a Python extension - this one: https://github.com/lauft/timew-report
+
+Please submit issues and any feedback you have if you are so inclined. I would like to make this extension more general and less specific to how I use it.
---
+++ csv.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+## ENV Variables & Defaults
+REPORT_PATH=${REPORT_PATH:-$HOME/timew-reports/}
+USER_NAME=${USER_NAME:-${USER}}
+SEP=${SEP:-';'}
+
+## Variables
+total_seconds=0
+report_file_name="${USER_NAME}-$(date -dlast-monday +%F)-weekly-log.csv"
+tmp_json=$(mktemp)
+tmp_csv=$(mktemp)
+
+## Create weekly log file
+mkdir -p "${REPORT_PATH}"
+[[ -e "${REPORT_PATH}"/"${report_file_name}" ]] && rm "${REPORT_PATH}"/"$Documents/SelfDesign/weekly-logs{report_file_name}"
+touch "${REPORT_PATH}"/"${report_file_name}"
+
+## Dependencies
+check_dependencies() {
+    ## An array of dependencies
+    depends=(timew jq)
+    ## Iterate through to verify that they are executable
+    for dep in "${depends[@]}"; do
+        if ! command -v "${dep}" >/dev/null 2>&1 ; then
+            echo "the dependency '${dep}' was not found"
+            exit 1
+        fi  
+    done
+}
+
+## Parsing the standard in from 'timew'
+parse_timew_data() {
+    data_section=false
+
+    ## Iterate through timew output looking for json
+    while read -r line; do
+        if [[ "${data_section}" == "true" ]]; then
+            echo "${line}" >> "${tmp_json}"
+        fi
+
+        ## When program reaches blank line denoting json, begin adding it to json file
+        [[ -z ${line} ]] && data_section=true
+    done <"${1}"
+}
+
+## Parse the Json created from 'timew'
+convert_json_to_csv() {
+    jq -r '(.[0] | keys_unsorted) as $keys | $keys, map([.[ $keys[] ] | walk(if type == "array" then join(" ") else . end) ])[] | @csv' "${tmp_json}" > "${tmp_csv}"
+}
+
+## Format Report
+reformat_csv() {
+    ## Add a header to the csv file and remove any old contents if old file exists
+    echo "ID${SEP}Tasks/Tags${SEP}Remarks/Notes${SEP}Date${SEP}Start${SEP}End${SEP}Duration${SEP}Totals" > "${REPORT_PATH}"/"${report_file_name}"
+
+    ## Iterate through tmp csv
+    while IFS="," read -r id start end tags notes; do
+
+        ## Modify and generate values for CSV
+        seconds=$( calc_duration "${start}" "${end}" )
+        duration=$( date -d@"${seconds}" -u +%H:%M:%S )
+        hmn_start=$( convert_time_to_human "${start}" )
+        hmn_end=$( convert_time_to_human "${end}" )
+        _date=$( convert_time_to_date "${start}" )
+        total_seconds=$((total_seconds + seconds))
+
+        ## Add modified values to csv
+        echo "${id}${SEP}${tags}${SEP}${notes}${SEP}${_date}${SEP}${hmn_start}${SEP}${hmn_end}${SEP}${duration}" >> "${REPORT_PATH}"/"${report_file_name}"
+
+    done < <(tail -n +2 "${tmp_csv}")
+
+    ## Add total hours to CSV on last line
+    echo "${SEP}${SEP}${SEP}${SEP}${SEP}${SEP}${SEP}$(convert_secs ${total_seconds})${SEP}(HH:MM)" >> "${REPORT_PATH}"/"${report_file_name}"
+}
+
+## Utility functions
+convert_time_to_unix(){
+    date -u -d "$(echo ${1//\"/} | sed -r 's|(.*)T(..)(..)(..)|\1 \2:\3:\4|; s|Z||')" +%s
+}
+
+convert_time_to_human() {
+    unix_sec=$(convert_time_to_unix "${1}")
+    date -d @"${unix_sec}" +%T 
+}
+
+convert_time_to_date() {
+    unix_sec=$(convert_time_to_unix "${1}")
+    date -d@"${unix_sec}" +%F
+}
+
+calc_duration(){
+    begin=$(convert_time_to_unix "${1}")
+    finish=$(convert_time_to_unix "${2}")
+    echo "$(($finish - $begin))"
+}
+convert_secs() {
+    ((h=${1}/3600))
+    ((m=(${1}%3600)/60))
+    ((s=${1}%60))
+    echo "${h}:${m}"
+}
+
+## Main Processes
+check_dependencies
+parse_timew_data "${1:-/dev/stdin}"
+convert_json_to_csv 
+reformat_csv
+
+echo -n "${REPORT_PATH}"/"${report_file_name}"
+
+clean_up() {
+    rm "${tmp_csv}"
+    rm "${tmp_json}"
+}
+
+trap clean_up exit
